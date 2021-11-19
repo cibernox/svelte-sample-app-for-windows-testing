c99c2b0961419ddf05ae2590c7647805d038fae4
Miguel Camba <miguel.camba@gmail.com>
2021-11-19 15:38:42 +0100

    Sample app for testing

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,15 @@
+<script context="module">
+  import { addMessages, init, getLocaleFromNavigator, register } from 'svelte-intl-precompile';
+  import en from '$locales/en.js';
+  import es from '$locales/es.js';
+
+  addMessages('en', en);
+  addMessages('es', es);
+
+  init({
+    fallbackLocale: 'en',
+    initialLocale: getLocaleFromNavigator()
+  });
+</script>
+
+<slot />
---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { t, locale } from 'svelte-intl-precompile';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Translation1: {$t("menu")}</p>
+<p>Translation2: {$t("foot", { values: { count: 2 } })}</p>
+
+<button type="button" on:click={() => $locale = 'en'}>EN</button>
+<button type="button" on:click={() => $locale = 'es'}>ES</button>
